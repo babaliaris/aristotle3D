6eec907bbefb5a98b35a0a8d2053cd42e5bc32df
babaliaris <babaliaris.nikos@gmail.com>
2026-03-09 18:55:17 +0200

    Removed a debugging line

--- Projects/Sandbox/src/sandbox.c
+++ Projects/Sandbox/src/sandbox.c
@@ -3,8 +3,6 @@
 
 ars3d_void ars3dUserEntryPoint(Ars3DApp *p_app)
 {
-    //ars3dDebugTestMacros();
-    ARS3D_ASSERT_SOFT(1 == 2, "%s", "1 is not equal to 2");
 }
 
 Ars3DApp * ars3dUserAppProvider()

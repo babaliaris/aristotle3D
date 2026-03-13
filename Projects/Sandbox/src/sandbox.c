383e1c296e5103404f650507b1f3c921144f8831
babaliaris <babaliaris.nikos@gmail.com>
2026-03-13 18:30:27 +0200

    Implemented CLI for sandbox to chose a layer to start

--- Projects/Sandbox/src/sandbox.c
+++ Projects/Sandbox/src/sandbox.c
@@ -1,6 +1,5 @@
 #define ARS3D_ENTRY_POINT
 #include <aristotle3D/aristotle3D.h>
-#include "sandbox.h"
 
 #include "layers/events-test.layer.h"
 #include "layers/render-triangle-test.layer.h"
@@ -8,81 +7,70 @@
 
 ars3d_void ars3dUserEntryPoint(Ars3DApp *p_app)
 {
-    ars3d_int argc              = ars3dAppGetProcessArgc(p_app);
-    ars3d_char **argv           = ars3dAppGetProcessArgv(p_app);
+    int choice = -1;
 
-    if ( argc == 2 )
-    {
-        ARS3D_INFO("Starting up LAYER: %s", argv[1]);
-
-        // Show the number of layers.
-        if ( ars3dStrCmp(argv[1], "list") == 0 )
-        {
-            __ars3dAppDestroy__(p_app);
-            showAvailableLayers();
-            ars3dExit(-1);
-        }
-
-        // LAYER: Test Events.
-        else if ( ars3dStrCmp(argv[1], SANDBOX_RUN_LAYER_TEST_EVENTS) == 0 )
-        {
-            EventsTestLayer *new_events_test_layer  = (EventsTestLayer *)ars3dMalloc( ARS3D_SIZEOF(EventsTestLayer) );
-            new_events_test_layer->m_counter        = 0;
-
-            ars3dAppAttachLayer(
-                p_app,
-                (ars3d_void *)new_events_test_layer,
-                onEventsTestLayerAttach,
-                onEventsTestLayerDetatch,
-                onEventsTestLayerStart,
-                onEventsTestLayerUpdate,
-                onEventsTestLayerEvent
-            );
-        }
-
-        // LAYER: Render Triangle.
-        else if ( ars3dStrCmp(argv[1], SANDBOX_RUN_LAYER_RENDER_TRIANGLE) == 0  )
-        {
-                RenderTriangleTestLayer *triangle_layer = (RenderTriangleTestLayer *)ars3dMalloc( ARS3D_SIZEOF(RenderTriangleTestLayer) );
-                ars3dAppAttachLayer(
-                    p_app,
-                    (ars3d_void *)triangle_layer,
-                    onRenderTriangleTestLayerAttach,
-                    onRenderTriangleTestLayerDetatch,
-                    onRenderTriangleTestLayerStart,
-                    onRenderTriangleTestLayerUpdate,
-                    onRenderTriangleTestLayerEvent
-                );
-        }
-
-        // LAYER: Barnsley.
-        else if ( ars3dStrCmp(argv[1], SANDBOX_RUN_LAYER_BARNSLEY) == 0  )
-        {
-                BarnsleyLayer *barnsley_layer = (BarnsleyLayer *)ars3dMalloc( ARS3D_SIZEOF(BarnsleyLayer) );
-                ars3dAppAttachLayer(
-                    p_app,
-                    (ars3d_void *)barnsley_layer,
-                    onBarnsleyLayerAttach,
-                    onBarnsleyLayerDetatch,
-                    onBarnsleyLayerStart,
-                    onBarnsleyLayerUpdate,
-                    onBarnsleyLayerEvent
-                );
-                
-        }
-
-        else
-        {
-            ARS3D_ERROR("Uknown run argument.");
-        }
+    // 1. Display the CLI Menu
+    printf("\n--- Aristotle3D Engine Sandbox ---\n");
+    printf("1. Test Events Layer\n");
+    printf("2. Render Triangle Test\n");
+    printf("3. Barnsley Fern Fractal\n");
+    printf("0. Exit\n");
+    printf("Select a layer to run: ");
+    
+    // Get the user's input.
+    if (scanf("%d", &choice) != 1) {
+        ARS3D_ERROR("Invalid input. Please enter a number.");
+        ars3dExit(-1);
     }
 
-    else
+    // Attach the chosen layer
+    switch (choice)
     {
-        ARS3D_ERROR("You did not provided a run argument.");
-        __ars3dAppDestroy__(p_app);
-        showAvailableLayers();
-        ars3dExit(-1);
+        case 1:
+        {
+            ARS3D_INFO("Starting up: Events Test");
+            EventsTestLayer *layer = (EventsTestLayer *)ars3dMalloc(sizeof(EventsTestLayer));
+            layer->m_counter = 0;
+            ars3dAppAttachLayer(
+                p_app, layer, onEventsTestLayerAttach, onEventsTestLayerDetatch, 
+                onEventsTestLayerStart, onEventsTestLayerUpdate, onEventsTestLayerEvent
+            );
+            break;
+        }
+
+        case 2:
+        {
+            ARS3D_INFO("Starting up: Triangle Render");
+            RenderTriangleTestLayer *layer = (RenderTriangleTestLayer *)ars3dMalloc(sizeof(RenderTriangleTestLayer));
+            ars3dAppAttachLayer(
+                p_app, layer, onRenderTriangleTestLayerAttach, onRenderTriangleTestLayerDetatch, 
+                onRenderTriangleTestLayerStart, onRenderTriangleTestLayerUpdate, onRenderTriangleTestLayerEvent
+            );
+            break;
+        }
+
+        case 3:
+        {
+            ARS3D_INFO("Starting up: Barnsley Fern");
+            BarnsleyLayer *layer = (BarnsleyLayer *)ars3dMalloc(sizeof(BarnsleyLayer));
+            ars3dAppAttachLayer(
+                p_app, layer, onBarnsleyLayerAttach, onBarnsleyLayerDetatch, 
+                onBarnsleyLayerStart, onBarnsleyLayerUpdate, onBarnsleyLayerEvent
+            );
+            break;
+        }
+
+        case 0:
+            ARS3D_INFO("Exiting...");
+            __ars3dAppDestroy__(p_app);
+            ars3dExit(0);
+            return;
+
+        default:
+            ARS3D_ERROR("Unknown choice. Exiting.");
+            __ars3dAppDestroy__(p_app);
+            ars3dExit(-1);
+            return;
     }
 }
 
